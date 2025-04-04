c3024305fc3419a03a728376eaeadbea8149c030
ShivamGit <shivampaliwal007@gmail.com>
2025-04-04 15:53:28 +0530

    Category click crash bug fix

--- app/src/main/java/com/example/foodapp/activities/CategoryDetailActivity.kt
+++ app/src/main/java/com/example/foodapp/activities/CategoryDetailActivity.kt
@@ -2,42 +2,45 @@ package com.example.foodapp.activities
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.foodapp.R
 import com.example.foodapp.adapters.CategoriesAdapter
-import com.example.foodapp.adapters.CategoriesItemsAdapter
 import com.example.foodapp.databinding.ActivityCategoryDetailBinding
-import com.example.foodapp.databinding.ActivityMealBinding
 import com.example.foodapp.models.CategoryMeals
-import com.example.foodapp.models.Meal
 import com.example.foodapp.viewModel.CategoryMealsViewModel
-import com.example.foodapp.viewModel.MealViewModel
 
 class CategoryDetailActivity : AppCompatActivity() {
 
     private  lateinit var binding: ActivityCategoryDetailBinding
     private lateinit var mealMvvm: CategoryMealsViewModel
-    private lateinit var categoriesItemsAdapter : CategoriesAdapter
+    private lateinit var categoriesAdapter : CategoriesAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_category_detail)
 
+        binding = ActivityCategoryDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        mealMvvm = ViewModelProvider(this)[CategoryMealsViewModel::class.java]
+
+        categoriesAdapter = CategoriesAdapter()
+
+        prepareCategoryItemsRecyclerView()
         observerCategoryDetailLivedData()
+
     }
 
     private fun prepareCategoryItemsRecyclerView() {
         binding.rvCategoryNames.apply {
             layoutManager = LinearLayoutManager(this@CategoryDetailActivity, LinearLayoutManager.HORIZONTAL, false)
-            adapter = categoriesItemsAdapter
+            adapter = categoriesAdapter
         }
     }
 
     private fun observerCategoryDetailLivedData(){
         mealMvvm.observerCategoryLiveData().observe(this,
             { mealList ->
-                categoriesItemsAdapter.setCategoryMeals(mealsList = mealList as ArrayList <CategoryMeals>)
+                categoriesAdapter.setCategoryMeals(mealsList = mealList as ArrayList <CategoryMeals>)
             })
     }
 }
